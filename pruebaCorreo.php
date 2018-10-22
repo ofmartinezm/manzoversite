1574a2b859fcef629c9a736055b0d97b85a8576c
ofmartinezm <oscar.masmela@gmail.com>
2018-10-22 12:42:14 -0500

    se adiciona el repositorio para el manejo de la pagina web de manzover

---
+++ pruebaCorreo.php
@@ -0,0 +1,62 @@
+
+    // ini_set( 'display_errors', 1 );
+    // error_reporting( E_ALL );
+    // $from = "serviciotecnico@manzover.co";
+    // $to = "servicioalcliente@manzover.co";
+    // $subject = "Prueba envio mail";
+    // $message = "Esta es una prueba que funciona la fucion mail() de PHP ";
+    // $headers = "From:" . $from;
+    // $respuesta= mail($to,$subject,$message, $headers);
+    // if($respuesta){
+    // echo ("El mensaje se envió correctamente...");
+    // }else{
+    //     echo ("El mensaje NO!!! se pudo eviar :".E_ALL);
+    // }
+    // $mail = "Prueba de mensaje";
+    // //Titulo
+    // $titulo = "PRUEBA DE TITULO";
+    // //cabecera
+    // $headers = "MIME-Version: 1.0\r\n"; 
+    // $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+    // //dirección del remitente 
+    // $headers .= "From: Geeky Theory < sevicioalcliente@manzover.co >\r\n";
+    // //Enviamos el mensaje a tu_dirección_email 
+    // $bool = mail("oscar.masmela@gmail.co",$titulo,$mail,$headers);
+    // if($bool){
+    //     echo "Mensaje enviado";
+    // }else{
+    //     echo "Mensaje no enviado";
+    // }
+    <!DOCTYPE HTML>
+<html lang="es">
+<head>
+<meta charset="utf-8"/>
+<title>Formulario</title>
+<link href="styles.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<!-- formulario de contacto -->
+
+	<form action="envia.php" method="post" class="form-consulta"> 
+		<label>Nombre y apellido: <span>*</span>
+			<input type="text" name="nombre" placeholder="Nombre y apellido" class="campo-form" required>
+		</label>
+		
+		<label>Email: <span>*</span>
+			<input type="email" name="email" placeholder="Email" class="campo-form" required>
+		</label>
+		
+		<label>Consulta:
+			<textarea name="consulta" class="campo-form"></textarea>
+		</label>
+
+		<input type="submit" value="Enviar" class="btn-form">
+	</form>
+
+<!-- formulario -->
+
+</body>
+</html>
---
+++ pruebaPHP.php
@@ -0,0 +1,28 @@
+<?php
+// Conectando, seleccionando la base de datos
+$link = mysql_connect('199.79.62.243:3306', 'manzover', 'mo12345')
+    or die('No se pudo conectar: ' . mysql_error());
+echo 'Connected successfully';
+// mysql_select_db('manzover_xiontech') or die('No se pudo seleccionar la base de datos');
+
+// // Realizar una consulta MySQL
+// $query = 'SELECT * FROM post';
+// $result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+
+// // Imprimir los resultados en HTML
+// echo "<table>\n";
+// while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+//     echo "\t<tr>\n";
+//     foreach ($line as $col_value) {
+//         echo "\t\t<td>$col_value</td>\n";
+//     }
+//     echo "\t</tr>\n";
+// }
+// echo "</table>\n";
+
+// // Liberar resultados
+// mysql_free_result($result);
+
+// // Cerrar la conexión
+// mysql_close($link);
+?>
